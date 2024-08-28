6d10d4b3f37b0105cc3f8f71bc25f07468f2b48f
yazeedk <36983323+yazeedk@users.noreply.github.com>
2024-08-28 18:28:29 +0300

    webpack update

--- cart-microfrontend/webpack.config.js
+++ cart-microfrontend/webpack.config.js
@@ -1,78 +1,71 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  // Entry point for the application
   entry: './src/index.js',
-
-  // Output configuration for Webpack
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     publicPath: '/',
   },
-
-  // Development server configuration
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'), // Serve static files from the public directory
+      directory: path.join(__dirname, 'public'),
     },
-    compress: true, // Enable gzip compression
-    port: 3001, // Specify the port for the dev server
-    historyApiFallback: true, // Enables support for history API routing
-    hot: true, // Enable hot module replacement
+    compress: true,
+    port: 3003,
+    historyApiFallback: true,
+    hot: true,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Enable CORS
+      'Access-Control-Allow-Origin': '*',
     },
   },
-
-  // Module rules for different file types
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, // Apply rule to .js and .jsx files
-        exclude: /node_modules/, // Exclude files in node_modules directory
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', // Use Babel loader to transpile JavaScript
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'], // Enable support for ES6+ and React
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
       {
-        test: /\.css$/, // Apply rule to .css files
-        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader to handle CSS files
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.(png|jpg|gif|svg)$/, // Apply rule to image files
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
-            loader: 'file-loader', // Use file-loader to handle image files
+            loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'images/', // Output images to the images directory
+              outputPath: 'images/',
             },
           },
         ],
       },
     ],
   },
-
-  // Plugins for additional functionality
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html', // Specify the HTML template file
-      filename: 'index.html', // Output filename for the HTML file
-      publicPath: '/', // Public path for resolving assets
+      template: './public/index.html',
+      filename: 'index.html',
+      publicPath: '/',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
     }),
     new webpack.DefinePlugin({
-      'process.env.PUBLIC_URL': JSON.stringify(''), // Define PUBLIC_URL as an empty string
+      'process.env.PUBLIC_URL': JSON.stringify(''),
     }),
   ],
-
-  // Resolve extensions for imports
   resolve: {
-    extensions: ['.js', '.jsx'], // Resolve these file extensions when importing
+    extensions: ['.js', '.jsx'],
   },
 };
