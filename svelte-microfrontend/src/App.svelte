756089bedd460b4523ca9f8984c818b041c75c82
mosaabodeh <79007089+mosaabodeh@users.noreply.github.com>
2024-08-30 14:51:39 +0300

    Add Authentecation Componant using Svelte

---
+++ svelte-microfrontend/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	import Router from 'svelte-spa-router';
+	import Signup from './Signup.svelte';
+	import Signin from './Signin.svelte';
+  
+	const routes = {
+	  '/signup': Signup,
+	  '/signin': Signin
+	};
+  </script>
+  
+  <main>
+	<Navbar />
+	<Router {routes} />
+  </main>
+  
+  <style>
+	main {
+	  padding: 20px;
+	}
+  </style>
+  
