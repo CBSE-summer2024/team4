583187f92b4b051b0796bd2ef4b7e9ab64f1a541
yazeedk <36983323+yazeedk@users.noreply.github.com>
2024-08-30 21:59:44 +0300

    update

--- cart-microfrontend/src/App.js
+++ cart-microfrontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Cart from './components/Cart';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Cart />
     </div>
   );
